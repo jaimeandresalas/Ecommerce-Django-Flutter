954eeb7c55c09ee457d4795c5d294f784e838b18
Jaime Andres Salas <jsalasmoreira@gmail.com>
2022-08-09 16:01:19 -0500

    Se agrega api rest

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Django rest framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
